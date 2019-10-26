82c4ab4a2bd7ee017e4ddb8773ba82e337c00c1c
Ellie Ang <git@angm.xyz>
2019-10-26 19:27:57 +0200

    Improve test runner to allow testing dirs and specifying point of failure (Also give it nicer output and make it fail properly when tests failed)

--- src/tests.rs
+++ src/tests.rs
@@ -1,6 +1,6 @@
 /*
  * Developed by Ellie Ang. (git@angm.xyz).
- * Last modified on 10/3/19 6:38 PM.
+ * Last modified on 10/26/19 7:28 PM.
  * This file is under the Apache 2.0 license. See LICENSE in the root of this repository for details.
  */
 
@@ -20,6 +20,15 @@ lazy_static! {
     };
 }
 
+/// All possible ways the compiler can fail compilation.
+#[derive(Debug, PartialEq)]
+enum Failure {
+    Parse,
+    Compile,
+    IR,
+    Panic,
+}
+
 #[no_mangle]
 extern "C" fn test_print(string: *const c_char) {
     let string = unsafe { CStr::from_ptr(string) };
@@ -35,55 +44,60 @@ extern "C" fn test_printnum(num: i64) {
 
 #[test]
 fn run_all() -> Result<(), ()> {
+    let (mut test_total, mut test_failed) = (0, 0);
     let mut test_path = env::current_dir().expect("Couldn't get current dir.");
     test_path.push("tests");
-    for_file(test_path);
-    Ok(())
+
+    let iter = test_path.read_dir().expect("Test path is a file?");
+    for file in iter.map(|f| f.unwrap().path()) {
+        match file.read_dir() {
+            Ok(iter) => iter.for_each(|file| run_test(file.expect("Couldn't get file.").path(), &mut test_total, &mut test_failed)),
+            Err(_) => run_test(file, &mut test_total, &mut test_failed)
+        }
+    }
+
+    println!("\n\nResult: {} out of {} tests succeeded.\n\n", (test_total - test_failed), test_total);
+    if test_failed == 0 { Ok(()) } else { Err(()) }
 }
 
-fn for_file(file: PathBuf) {
-    if file.is_dir() {
-        for file in file.read_dir().expect("Couldn't get current dir.") {
-            for_file(file.expect("Couldn't get file.").path());
-        }
-    } else {
-        println!("Running test: {}", file.to_str().unwrap());
-        let source = read_to_string(file.clone()).expect("Couldn't get file.");
-        let expected = get_expected_result(&source);
-        let result = panic::catch_unwind(|| exec_jit(file)).unwrap_or(Err(()));
+fn run_test(path: PathBuf, total: &mut usize, failed: &mut usize) {
+    *total += 1;
+    println!("Running test #{}: {}", total, relative_path(&path));
+    let expected = get_expected_result(path.clone());
+    let result = panic::catch_unwind(|| exec_jit(path)).unwrap_or(Err(Failure::Panic));
 
-        if result != expected {
-            println!(
-                "Test failed!\nResult:   {:?}\nExpected: {:?}\n",
-                result, expected
-            );
-        }
+    if result != expected {
+        *failed += 1;
+        println!(
+            "Test failed!\nResult:   {:?}\nExpected: {:?}\n",
+            result, expected
+        );
     }
 }
 
-fn exec_jit(file: PathBuf) -> Result<String, ()> {
-    let mut code = super::parse_source(vec![file, STD_LIB.lock().unwrap().clone()])
+fn exec_jit(path: PathBuf) -> Result<String, Failure> {
+    let mut code = super::parse_source(vec![path, STD_LIB.lock().unwrap().clone()])
         .ok()
-        .ok_or(())?;
+        .ok_or(Failure::Parse)?;
     super::auto_import_prelude(&mut code);
-    let mir = super::compile_mir(code).ok().ok_or(())?;
+    let mir = super::compile_mir(code).ok().ok_or(Failure::Compile)?;
     let module = super::compile_ir(mir);
 
     let engine = module
         .create_jit_execution_engine(OptimizationLevel::None)
         .ok()
-        .ok_or(())?;
+        .ok_or(Failure::IR)?;
     engine.add_global_mapping(
-        &module.get_function("print").ok_or(())?,
+        &module.get_function("print").ok_or(Failure::IR)?,
         test_print as usize,
     );
     engine.add_global_mapping(
-        &module.get_function("printnum").ok_or(())?,
+        &module.get_function("printnum").ok_or(Failure::IR)?,
         test_printnum as usize,
     );
 
     unsafe {
-        let main_fn: JitFunction<MainFn> = engine.get_function("main").ok().ok_or(())?;
+        let main_fn: JitFunction<MainFn> = engine.get_function("main").ok().ok_or(Failure::IR)?;
         main_fn.call();
     }
 
@@ -93,11 +107,24 @@ fn exec_jit(file: PathBuf) -> Result<String, ()> {
     Ok(result_copy)
 }
 
-fn get_expected_result(code: &String) -> Result<String, ()> {
-    if code.starts_with("// ERR") {
-        Err(())
+fn get_expected_result(mut path: PathBuf) -> Result<String, Failure> {
+    // If the test is a directory, the wanted result is in a file 'expected' in the dir
+    if path.is_dir() {
+        path.push("expected");
+    }
+
+    let code = read_to_string(path).expect("Couldn't get wanted result");
+    if code.starts_with("// P-ERR") {
+        Err(Failure::Parse)
+    } else if code.starts_with("// C-ERR") {
+        Err(Failure::Compile)
     } else {
         let split: Vec<&str> = code.split("*/").collect();
         Ok(split[0][3..].to_string())
     }
+}
+
+fn relative_path(path: &PathBuf) -> String {
+    // Unwrap it all!
+    format!("{}/{}", path.parent().unwrap().file_name().unwrap().to_str().unwrap(), path.file_name().unwrap().to_str().unwrap())
 }
